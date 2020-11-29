a51cb4bcc4ae34f6c88c2c1e136f1e19f6b5ca69
amogh <am210633@dal.ca>
2020-11-29 09:54:28 -0400

    Added form changes to Users Page

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import { Container } from "react-bootstrap";
 import Jumbotron from 'react-bootstrap/Jumbotron'
 
-let flag = 0;
+let flag=0;
 
 const validateForm = (errors) => {
     let valid = false;
     Object.values(errors).forEach(      // if we have an error string set valid to false
       (val) => 
-      { if(val=='set' && flag == 1)
+      { if(val ==='set' && flag === 1)
             { valid = true; }
         else
             { valid = false; }
@@ -17,18 +18,7 @@ const validateForm = (errors) => {
     return valid;
     }
 
-const countErrors = (errors) => {
-        let count = 0;
-        Object.values(errors).forEach(
-          (val) => {if(val.length > 0)
-            {
-                if(val!=='set'){
-                    (count = count+1);
-                }
-            } }
-        );
-        return count;
-      }
+
 
 class UserForm extends React.Component {
     constructor(props) {
@@ -55,8 +45,41 @@ class UserForm extends React.Component {
     }
   
     handleChange(event) {
-        this.setState({ [event.target.name]: event.target.value });
-        console.log("Set this :", this.state );
+        event.preventDefault();
+        const { name, value } = event.target;
+        let errors = this.state.errors;
+
+        if(name ==='food' || name === 'location'){
+            if((!event.target.value.match(/^[a-zA-Z ]+$/i)))
+                {
+                    event.target.value = event.target.value.replace(/[^A-Za-z ]/ig, '')
+                }
+                else{
+                    errors[name] = value.length < 5
+                    ? 'Enter 5 characters atleast!'
+                    : 'set';
+                    if (value.length>=5) {flag = 1;} else{ flag=0;}
+                }
+        }
+
+        else if(name ==='number' || name ==='quantity'){
+            if((!event.target.value.match(/^[0-9 ]+$/i)))
+                {
+                    event.target.value = event.target.value.replace(/[^0-9 ]/ig, '')
+                }
+                else{
+                    errors[name] = value < 1
+                    ? 'Minimum number cannot be less than 1'
+                    : 'set';
+                    if (value.length>=1) {flag = 1;} else{ flag=0;}
+                }            
+        }
+
+        this.setState({errors, [name]: value}, ()=> {
+            console.log(errors)
+        })
+
+        
     }
   
     handleSubmit(event) {
@@ -80,8 +103,8 @@ class UserForm extends React.Component {
                     <form onSubmit={this.handleSubmit}>
                   
                         <div className="form-group">
-                            <label >What time did you feed the ducks?</label>
-                            <input type="text" className="form-control" name="feedtime" id="feedtime" onChange={this.handleChange}  placeholder="Enter email"/>                            
+                            <label >What time did you feed the ducks?</label>                            
+                            <input  type="time" name="feedtime" step="1" value={this.state.time} className="form-control" placeholder="Time" onChange={this.handleChange}/>                         
                         </div>
                         <div className="form-group">
                             <label >What did you feed?</label>
